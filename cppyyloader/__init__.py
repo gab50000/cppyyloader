0c0ad1acf501e9b6faa978d8bcc13ae29b32e257
Gabriel Kabbe <gabriel.kabbe@mail.de>
2022-01-12 21:33:55 +0100

    Simplify even more

--- cppyyloader/__init__.py
+++ cppyyloader/__init__.py
@@ -3,13 +3,12 @@ __version__ = "0.0.1"
 
 import sys
 
-import cppyy
-
 
 __all__ = ("std",)
 
 
 def __getattr__(name):
-    gbl = __import__(f"cppyy.gbl", fromlist=[""])
+    from cppyy import gbl
+
     mod = getattr(gbl, name)
     return mod
